956231dff7ce683f12efa6b00599436589ddae67
Luca Andriotto <luca.andriotto1994@gmail.com>
2024-05-24 10:46:43 +0200

    Aggiornamento dipendenze pacchetti Julia.

--- src/util/graphs_trigger.jl
+++ src/util/graphs_trigger.jl
@@ -19,23 +19,25 @@ theme(:solarized)
 
 ## Limiti su x
 ### Alveoli
-xlims_al = tspan
+xlims_al = (0, .1)
 ### Airways
-xlims_aw = tspan
+xlims_aw = (0, .1)
 
 ## Limiti su y
 ### Alveoli
-ylims_al = (-2e-6, 2e-6)
+ylims_al = (0, 1)
 ### Airways
-ylims_aw = (-5e-6, 5e-6)
+ylims_aw = (0, 1)
 
+l = (5, 1)
+# l = @layout [a b; c d; e;]
 
 ## Alveoli
 IAE_pl = plot(sol,
     idxs=[
-        system.IAE.trigger_in,
-        system.IAE.trigger_out,
-        system.IAE.∫i,
+        system.L.IAE.i1.trigger_in,
+        system.L.IAE.i1.trigger_out,
+        system.L.IAE.i1.n∫i,
     ],
     title="IAE",
     xlabel="t [s]", ylabel="",
@@ -49,9 +51,9 @@ IAE_pl = plot(sol,
 
 IAI_pl = plot(sol,
     idxs=[
-        system.IAI.trigger_in,
-        system.IAI.trigger_out,
-        system.IAI.∫i,
+        system.L.IAI.i1.trigger_in,
+        system.L.IAI.i1.trigger_out,
+        system.L.IAI.i1.n∫i,
     ],
     title="IAI",
     xlabel="t [s]", ylabel="",
@@ -65,9 +67,9 @@ IAI_pl = plot(sol,
 
 IBB_pl = plot(sol,
     idxs=[
-        system.IBB.trigger_in,
-        system.IBB.trigger_out,
-        system.IBB.∫i,
+        system.L.IBB.i1.trigger_in,
+        system.L.IBB.i1.trigger_out,
+        system.L.IBB.i1.n∫i,
     ],
     title="IBB",
     xlabel="t [s]", ylabel="",
@@ -81,9 +83,9 @@ IBB_pl = plot(sol,
 
 IAG_pl = plot(sol,
     idxs=[
-        system.IAG.trigger_in,
-        system.IAG.trigger_out,
-        system.IAG.∫i,
+        system.L.IAG.i1.trigger_in,
+        system.L.IAG.i1.trigger_out,
+        system.L.IAG.i1.n∫i,
     ],
     title="IAG",
     xlabel="t [s]", ylabel="",
@@ -97,9 +99,9 @@ IAG_pl = plot(sol,
 
 IBA_pl = plot(sol,
     idxs=[
-        system.IBA.trigger_in,
-        system.IBA.trigger_out,
-        system.IBA.∫i,
+        system.L.IBA.i1.trigger_in,
+        system.L.IBA.i1.trigger_out,
+        system.L.IBA.i1.n∫i,
     ],
     title="IBA",
     xlabel="t [s]", ylabel="",
@@ -111,9 +113,7 @@ IBA_pl = plot(sol,
     margin=1cm,
     size=(600, 600))
 
-l = @layout [a b; c d; e]
-
-al_pl = plot(IAE_pl, IAI_pl, IBB_pl, IAG_pl, IBA_pl,
+al_trig_pl = plot(IAE_pl, IAI_pl, IBB_pl, IAG_pl, IBA_pl,
     plot_title="Simulazione [Alveoli] (A = $(resp_ampl)V)",
     dpi=300,
     layout=l,
@@ -123,9 +123,9 @@ al_pl = plot(IAE_pl, IAI_pl, IBB_pl, IAG_pl, IBA_pl,
 
 IBL_pl = plot(sol,
     idxs=[
-        system.IBL.trigger_in,
-        system.IBL.trigger_out,
-        system.IBL.∫i,
+        system.L.IBL.i1.trigger_in,
+        system.L.IBL.i1.trigger_out,
+        system.L.IBL.i1.n∫i,
     ],
     title="IBL",
     xlabel="t [s]", ylabel="",
@@ -139,9 +139,9 @@ IBL_pl = plot(sol,
 
 IAH_pl = plot(sol,
     idxs=[
-        system.IAH.trigger_in,
-        system.IAH.trigger_out,
-        system.IAH.∫i,
+        system.L.IAH.i1.trigger_in,
+        system.L.IAH.i1.trigger_out,
+        system.L.IAH.i1.n∫i,
     ],
     title="IAH",
     xlabel="t [s]", ylabel="",
@@ -155,9 +155,9 @@ IAH_pl = plot(sol,
 
 IAF_pl = plot(sol,
     idxs=[
-        system.IAF.trigger_in,
-        system.IAF.trigger_out,
-        system.IAF.∫i,
+        system.L.IAF.i1.trigger_in,
+        system.L.IAF.i1.trigger_out,
+        system.L.IAF.i1.n∫i,
     ],
     title="IAF",
     xlabel="t [s]", ylabel="",
@@ -171,9 +171,9 @@ IAF_pl = plot(sol,
 
 IAD_pl = plot(sol,
     idxs=[
-        system.IAD.trigger_in,
-        system.IAD.trigger_out,
-        system.IAD.∫i,
+        system.L.IAD.i1.trigger_in,
+        system.L.IAD.i1.trigger_out,
+        system.L.IAD.i1.n∫i,
     ],
     title="IAD",
     xlabel="t [s]", ylabel="",
@@ -185,9 +185,10 @@ IAD_pl = plot(sol,
     margin=1cm,
     size=(600, 600))
 
-l = @layout [a b; c d]
+l = (4, 1)
+# l = @layout [a b; c d]
 
-aw_pl = plot(IAD_pl, IAF_pl, IAH_pl, IBL_pl,
+aw_trig_pl = plot(IAD_pl, IAF_pl, IAH_pl, IBL_pl,
     plot_title="Simulazione [Airways] (A = $(resp_ampl)V)",
     dpi=300,
     layout=l,
@@ -197,7 +198,10 @@ aw_pl = plot(IAD_pl, IAF_pl, IAH_pl, IBL_pl,
  SALVATAGGIO FIGURE
  ===============================================================#
 
-savefig(al_pl,
+savefig(al_trig_pl,
         "$srcdir/output/trig_integ_al_$(resp_ampl)_tspan_$(tspan).png")
-savefig(aw_pl,
+savefig(aw_trig_pl,
         "$srcdir/output/trig_integ_aw_$(resp_ampl)_tspan_$(tspan).png")
+
+display(al_trig_pl)
+display(aw_trig_pl)
